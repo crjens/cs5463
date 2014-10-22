d3ac79e3523d9f250b038dbd7499fee3ea0ff8ec
Craig Jensen <crjens@hotmail.com>
2014-10-22 13:34:00 -0700

    Install either on RPi or BPi

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
- 
-git clone git://git.drogon.net/wiringPi
-cd ./wiringPi 
+
+if lscpu | grep Architecture | grep -q armv6 
+then 
+  # Raspberry Pi 
+  git clone git://git.drogon.net/wiringPi 
+  cd ./wiringPi 
+else 
+  # Banana Pi
+  git clone https://github.com/LeMaker/WiringBPi.git 
+  cd ./WiringBPi 
+fi 
 ./build 
 cd ../ 
 node-gyp rebuild 
