420fc35373cec031557e0f6c22467503ef904f47
Craig Jensen <cjensen@microsoft.com>
2019-01-19 22:37:33 -0800

    configure spi

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ cd ./wiringPi
 ./build 
 cd ../ 
 node-gyp rebuild 
+
+# enable SPI
+echo '>>> Enable SPI'
+sudo raspi-config nonint do_spi 0
+#sudo sed -i 's/^#dtparam=spi=on.*/dtparam=spi=on/' /boot/config.txt
