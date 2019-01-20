6df5f479f91f4425ce18420976e672b1aead2738
Craig Jensen <cjensen@microsoft.com>
2019-01-20 11:19:23 -0800

    update install script

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Raspberry Pi 
+# require root
+if [ $(id -u) -ne 0 ]; then
+  printf "Script must be run as root. Try 'sudo $0'\n"
+  exit 1
+fi
+
+# Install wiring Pi
+echo '>>> Install Wiring Pi'
 git clone git://git.drogon.net/wiringPi 
 cd ./wiringPi 
 ./build 
 cd ../ 
-node-gyp rebuild 
+node-gyp rebuild  
 
 # enable SPI
 echo '>>> Enable SPI'
 sudo raspi-config nonint do_spi 0
-#sudo sed -i 's/^#dtparam=spi=on.*/dtparam=spi=on/' /boot/config.txt
